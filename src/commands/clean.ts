3aca250688f1352c38191d145b79883ffc90cd31
somni <me@somni.one>
2022-10-15 00:03:19 +0900

    Add `--wet` parameter for `clean` command

--- src/commands/clean.ts
+++ src/commands/clean.ts
@@ -1,5 +1,5 @@
 import CLU from "command-line-usage";
-import { Command } from "@/lib/command";
+import { Command, Param } from "@/lib/command";
 
 export default class Clean extends Command {
   get helpMessage(): string {
@@ -8,12 +8,19 @@ export default class Clean extends Command {
         header: "Cleaner (`clean`)",
         content: [
           "Clean Tweets using Tweet Archive.",
-          "\nThis command needs user access token secrets for accessing Twitter API, which can be obtained/saved (to file) by command {bold auth}."
+          "\nThis command needs user access token secrets for accessing Twitter API, which can be obtained/saved (to file) by command {bold auth}.",
+          "\nThis command will always perform dry run unless {underline --wet} parameter is specified, to prevent the user clean their Tweets by mistake.",
+          "\n\n{red {bold CAUTION}: USE THIS COMMAND ON YOUR OWN RISK. DEVELOPER IS NOT RESPONSIBLE FOR ANY LOSS OR/AND DAMAGE OF YOUR DATA.}"
         ]
       },
       {
         header: "Parameters",
         optionList: [
+          {
+            name: "wet",
+            description: "Perform real cleaning job.",
+            type: Boolean,
+          },
           {
             name: "help",
             description: "Print this help message.",
@@ -25,8 +32,9 @@ export default class Clean extends Command {
   }
 
   get availableParams() {
-    // TODO
-    return { };
+    return {
+      "wet": new Param("wet"),
+    };
   }
 
   async doCommand(args: string[]): Promise<boolean> {
