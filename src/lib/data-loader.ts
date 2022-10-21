8c5b9ca513c2bcde7b1cfd2e3b3435a9c3ce01e7
somni <me@somni.one>
2022-10-21 22:22:07 +0900

    Remove file availability check before `readFile`

--- src/lib/data-loader.ts
+++ src/lib/data-loader.ts
@@ -2,14 +2,14 @@ import * as fs from "fs/promises";
 import { TweetEssential } from "./interfaces";
 
 export async function loadTweetsJs(path: string): Promise<TweetEssential[] | null> {
+  let rawText = "";
+
   try {
-    await fs.access(path, fs.constants.F_OK);
+    rawText = await fs.readFile(path, { encoding: "utf8", flag: "r" });
+    if(!rawText) return null;
   } catch {
     return null;
   }
-
-  const rawText = await fs.readFile(path, { encoding: "utf8", flag: "r" });
-  if(!rawText) return null;
 
   const fixedText = rawText.trim().replaceAll(/^window\.YTD\.tweets\.part\d+\s?=\s?/g, "");
   if(!fixedText.startsWith("[")) return null;
