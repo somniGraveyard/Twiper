231384eca2bcbb4e7596ed76a5c99f6722e278af
somni <me@somni.one>
2022-12-16 01:03:57 +0900

    Add ability to parse string-valued params for `Param`

--- src/lib/command.ts
+++ src/lib/command.ts
@@ -6,7 +6,7 @@ export class Param {
   private _name = "";
   private _alias: string | undefined = undefined;
   private _helpDescription = "";
-  private _helpType = Boolean;
+  private _helpType: any = Boolean;
 
   constructor(options: {
     name: string,
@@ -36,6 +36,14 @@ export class Param {
     }
   }
 
+  get valueType(): "boolean" | "string" | "any" {
+    switch(this._helpType) {
+      case Boolean: return "boolean";
+      case String: return "string";
+      default: return "any";
+    }
+  }
+
   get helpDefinition(): CLU.OptionDefinition {
     return {
       name: this._name,
@@ -51,6 +59,22 @@ export class Param {
       return true;
     } else {
       return false;
+    }
+  }
+
+  getParamValue(argList: string[]): string | null {
+    if(this.hasParam(argList) && this.valueType === "string") {
+      const paramIndex = (argList.indexOf(this.nameParam) + 1 || (this.aliasParam ? argList.indexOf(this.aliasParam) + 1 : 0)) - 1;
+
+      if(paramIndex >= 0 &&
+         paramIndex + 1 < argList.length &&
+         !argList[paramIndex + 1].startsWith("-")) {
+        return argList[paramIndex + 1];
+      } else {
+        return null;
+      }
+    } else {
+      return null;
     }
   }
 }
@@ -99,13 +123,43 @@ export abstract class Command {
   }
 
   /* === Predefined functions === */
+  findParamByArg(arg: string): Param | null {
+    for(const paramKey in this.availableParams) {
+      const param = this.availableParams[paramKey];
+      if(param.hasParam([arg])) return param;
+    }
+
+    return null;
+  }
+
   commandEntry(args: string[]): boolean {
+    let willHaveParamValue = false;
+
     // Show error message and exit if unknown parameter exists
     for(const arg of args) {
+      if(this.availableParamsFlatten.includes(arg) && this.findParamByArg(arg)?.valueType === "string") {
+        willHaveParamValue = true;
+      }
+
       if(!this.availableParamsFlatten.includes(arg)) {
+        if(willHaveParamValue) {
+          if(!arg.startsWith("-")) {
+            willHaveParamValue = false;
+            continue;
+          } else if(arg.startsWith("-")) {
+            L.e(this.name, chalk`Parameter needs string value`);
+            return false;
+          }
+        }
+
         L.e(this.name, chalk`Unknown parameter: {bold ${arg}}`);
         return false;
       }
+    }
+
+    if(willHaveParamValue) {
+      L.e(this.name, chalk`Parameter needs string value`);
+      return false;
     }
 
     return true;
